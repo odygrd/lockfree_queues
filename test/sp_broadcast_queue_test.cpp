e13ce2f03e66ba6e80913eecfb6b67216c5ce99f
Odysseas Georgoudis <odygrd@gmail.com>
2023-10-30 12:32:32 +0000

    sp broadcast queue add tests

--- test/sp_broadcast_queue_test.cpp
+++ test/sp_broadcast_queue_test.cpp
@@ -3,6 +3,7 @@
 #include "lockfree_queues/sp_broadcast_queue.h"
 
 #include <array>
+#include <memory>
 #include <set>
 #include <thread>
 #include <vector>
@@ -15,6 +16,13 @@ using namespace lockfree_queues;
 struct SPBQTestType
 {
   static std::set<SPBQTestType const*> constructed;
+
+  SPBQTestType(uint32_t v) noexcept
+  {
+    x = v;
+    REQUIRE_EQ(constructed.count(this), 0);
+    constructed.insert(this);
+  };
 
   SPBQTestType() noexcept
   {
@@ -55,6 +63,8 @@ struct SPBQTestType
     REQUIRE_EQ(constructed.count(this), 1);
     constructed.erase(this);
   };
+
+  uint32_t x;
 };
 
 std::set<const SPBQTestType*> SPBQTestType::constructed;
@@ -128,97 +138,150 @@ TEST_CASE("over_subscribe")
 /***/
 TEST_CASE("single_produce_single_consumers")
 {
-  // Fuzz and performance test
-  {
-    const size_t iter = 1'000'000;
-    SPBroadcastQueue<size_t> q{1024};
+  const size_t iter = 1'000'000;
+  SPBroadcastQueue<size_t> q{1024};
 
-    std::atomic<bool> flag(false);
-    std::thread producer{[&q, &flag, iter]
+  std::atomic<bool> flag(false);
+  std::thread producer{[&q, &flag, iter]
+                       {
+                         while (!flag)
+                           ;
+
+                         for (size_t i = 0; i < iter; ++i)
                          {
-                           while (!flag)
-                             ;
+                           q.emplace(i);
+                         }
+                       }};
 
-                           for (size_t i = 0; i < iter; ++i)
-                           {
-                             q.emplace(i);
-                           }
-                         }};
-
-    size_t rid = q.subscribe();
-    size_t sum = 0;
-    flag = true;
-    for (size_t i = 0; i < iter; ++i)
-    {
-      while (!q.front(rid))
-        ;
-      sum += *q.front(rid);
-      q.pop(rid);
-    }
-
-    REQUIRE_EQ(q.front(rid), nullptr);
-    REQUIRE_EQ(sum, iter * (iter - 1) / 2);
-    q.unsubscribe(rid);
-
-    producer.join();
+  size_t rid = q.subscribe();
+  size_t sum = 0;
+  flag = true;
+  for (size_t i = 0; i < iter; ++i)
+  {
+    while (!q.front(rid))
+      ;
+    sum += *q.front(rid);
+    q.pop(rid);
   }
+
+  REQUIRE_EQ(q.front(rid), nullptr);
+  REQUIRE_EQ(sum, iter * (iter - 1) / 2);
+  q.unsubscribe(rid);
+
+  producer.join();
 }
 
 /***/
 TEST_CASE("single_produce_multiple_consumers")
 {
-  // Fuzz and performance test
-  {
-    const size_t iter = 1'000'000;
-    constexpr size_t MAX_CONSUMERS = 4;
-    SPBroadcastQueue<size_t, MAX_CONSUMERS> q{1024};
+  // All Consumers subscribe before producer starts producing
+  const size_t iter = 1'000'000;
+  constexpr size_t MAX_CONSUMERS = 4;
+  SPBroadcastQueue<size_t, MAX_CONSUMERS> q{1024};
 
-    std::array<std::atomic<bool>, MAX_CONSUMERS> flags = {false};
+  std::array<std::atomic<bool>, MAX_CONSUMERS> flags = {false};
 
-    std::thread producer{[&q, &flags, iter]()
+  std::thread producer{[&q, &flags, iter]()
+                       {
+                         for (auto const& flag : flags)
                          {
-                           for (auto const& flag : flags)
-                           {
-                             while (!flag)
-                               ;
-                           }
+                           while (!flag)
+                             ;
+                         }
 
-                           for (size_t i = 0; i < iter; ++i)
-                           {
-                             q.emplace(i);
-                           }
-                         }};
+                         for (size_t i = 0; i < iter; ++i)
+                         {
+                           q.emplace(i);
+                         }
+                       }};
 
-    std::vector<std::thread> consumers;
-    for (size_t tid = 0; tid < MAX_CONSUMERS; ++tid)
-    {
-      consumers.emplace_back(
-        [&q, &flags, tid, iter]()
+  std::vector<std::thread> consumers;
+  for (size_t tid = 0; tid < MAX_CONSUMERS; ++tid)
+  {
+    consumers.emplace_back(
+      [&q, &flags, tid, iter]()
+      {
+        size_t rid = q.subscribe();
+        size_t sum = 0;
+        flags[tid] = true;
+
+        for (size_t i = 0; i < iter; ++i)
         {
-          size_t rid = q.subscribe();
-          size_t sum = 0;
-          flags[tid] = true;
+          while (!q.front(rid))
+            ;
+          sum += *q.front(rid);
+          q.pop(rid);
+        }
 
-          for (size_t i = 0; i < iter; ++i)
-          {
-            while (!q.front(rid))
-              ;
-            sum += *q.front(rid);
-            q.pop(rid);
-          }
-
-          REQUIRE_EQ(q.front(rid), nullptr);
-          REQUIRE_EQ(sum, iter * (iter - 1) / 2);
-          q.unsubscribe(rid);
-        });
-    }
-
-    for (auto& c : consumers)
-    {
-      c.join();
-    }
-    producer.join();
+        REQUIRE_EQ(q.front(rid), nullptr);
+        REQUIRE_EQ(sum, iter * (iter - 1) / 2);
+        q.unsubscribe(rid);
+      });
   }
+
+  for (auto& c : consumers)
+  {
+    c.join();
+  }
+  producer.join();
 }
 
+/***/
+TEST_CASE("single_produce_multiple_consumers_subscribe")
+{
+  // Consumers subscribe after the producer starts
+  const size_t iter = 1'000'000;
+  constexpr size_t MAX_CONSUMERS = 4;
+
+  SPBroadcastQueue<SPBQTestType, MAX_CONSUMERS> q{1024};
+
+  std::thread producer{[&q, iter]()
+                       {
+                         for (size_t i = 0; i < iter; ++i)
+                         {
+                           q.emplace(i);
+                         }
+                       }};
+
+  // let the producer thread start,
+  // the producer waits for at least one consumer before it is able to emplace
+  std::this_thread::sleep_for(std::chrono::nanoseconds{200});
+
+  std::vector<std::thread> consumers;
+  for (size_t tid = 0; tid < MAX_CONSUMERS; ++tid)
+  {
+    consumers.emplace_back(
+      [&q, iter]()
+      {
+        size_t rid = q.subscribe();
+
+        while (true)
+        {
+          SPBQTestType const* item = q.front(rid);
+          while (!item)
+          {
+            item = q.front(rid);
+          }
+
+          if (item->x == iter - 1)
+          {
+            // last item
+            q.pop(rid);
+            break;
+          }
+
+          q.pop(rid);
+        }
+
+        REQUIRE_EQ(q.front(rid), nullptr);
+        q.unsubscribe(rid);
+      });
+  }
+
+  for (auto& c : consumers)
+  {
+    c.join();
+  }
+  producer.join();
+}
 TEST_SUITE_END();
